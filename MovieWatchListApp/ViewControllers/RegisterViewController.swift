3f7086259d87c1e2c2312b3286debbd42de9da31
Damian Ivanov <37500930+damianivanov@users.noreply.github.com>
2022-02-10 13:48:48 +0200

    Field Validation on register

--- MovieWatchListApp/ViewControllers/RegisterViewController.swift
+++ MovieWatchListApp/ViewControllers/RegisterViewController.swift
@@ -17,6 +17,56 @@ class RegisterViewController : UIViewController {
         registerButton.layer.cornerRadius = 30
     }
 
+    
+    func validateFields() -> String? {
+        if usernameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            confirmPasswordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all fields"
+        }
+        
+        //Valid Password
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in:
+            .whitespacesAndNewlines)
+        
+        if !Utilities.isPasswordValid(cleanedPassword) {
+            return "Wrong password format"
+        }
+        
+        // Valid Email
+        let cleanedEmail = emailTextField.text!.trimmingCharacters(in:
+            .whitespacesAndNewlines)
+        
+        if !Utilities.isEmailValid(cleanedEmail) {
+            return "Invalid email"
+        }
+        
+        // Confirmed Password
+        let cleanedConfirmPassword = confirmPasswordTextField.text!.trimmingCharacters(in:
+            .whitespacesAndNewlines)
+        
+        if cleanedPassword != cleanedConfirmPassword {
+            return "Passwords do not match"
+        }
+        
+        return nil
+    }
+    
     @IBAction func RegisterPressed(_ sender: Any) {
+        
+        let error = validateFields()
+        
+        if error != nil {
+            // There is an error
+             showError(error!)
+        } else {
+          // create user
+        }
+    }
+    
+    func showError(_ message:String) {
+        // errorLabel.text = message
+        // errorLabel.alpha = 1
     }
 }
