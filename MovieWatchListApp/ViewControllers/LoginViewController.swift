e8303e066ce174bbd131fe9021974c401e4775db
Damian Ivanov <37500930+damianivanov@users.noreply.github.com>
2022-02-18 13:23:18 +0200

    fixed animation,auto completion,emailText field

--- MovieWatchListApp/ViewControllers/LoginViewController.swift
+++ MovieWatchListApp/ViewControllers/LoginViewController.swift
@@ -5,7 +5,7 @@ class LoginViewController : UIViewController {
     
     @IBOutlet weak var loginFormView: UIView!
     @IBOutlet weak var logo: UIImageView!
-    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var LoginButton: UIButton!
     @IBOutlet weak var RegisterButton: UIButton!
@@ -19,19 +19,63 @@ class LoginViewController : UIViewController {
         RegisterButton.layer.cornerRadius = 30
         logo.layer.cornerRadius = logo.frame.height/2
     }
-    @IBAction func LoginPressed(_ sender: Any) {
-        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
-        let cleanedEmail = usernameTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
-        Auth.auth().signIn(withEmail: cleanedEmail, password: cleanedPassword) { [weak self]  authResult, error in
-            if error != nil{
-                debugPrint(error)
-            } else{
-                    self?.performSegue(withIdentifier: "login", sender: sender)
-            }
-            
+    
+    func validateFields() -> String? {
+        if  emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+                passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all fields"
         }
+        //Valid Password
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+        if !Utilities.isPasswordValid(cleanedPassword) {
+            return "Wrong password format"
+        }
+        
+        // Valid Email
+        let cleanedEmail = emailTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+        
+        if !Utilities.isEmailValid(cleanedEmail) {
+            return "Invalid email"
+        }
+        
+        return nil
     }
-    @IBAction func RegisterPressed(_ sender: Any) {
-        performSegue(withIdentifier: "openRegister", sender: sender)
+    
+    @IBAction func LoginPressed(_ sender: Any) {
+        activityIndicator.startAnimating()
+        let error = validateFields()
+        if error != nil {
+            // There is an error
+            showError(error!)
+        }else{
+            let cleanedPassword = passwordTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+            let cleanedEmail = emailTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+            Auth.auth().signIn(withEmail: cleanedEmail, password: cleanedPassword) { authResult, error in
+                if error != nil{
+                    debugPrint(error)
+                    self.showError(error!.localizedDescription)
+                } else {
+                    let user = Auth.auth().currentUser
+                    if let user = user {
+                        let uid = user.uid
+                        let email = user.email
+                        print(uid,email)
+                    }
+                    print(user)
+                    self.performSegue(withIdentifier: "login", sender: sender)
+                }
+                
+            }
+        
     }
+}
+    
+@IBAction func RegisterPressed(_ sender: Any) {
+    performSegue(withIdentifier: "openRegister", sender: sender)
+}
+func showError(_ message:String) {
+    validationMessage.text = message
+    validationMessage.alpha = 1
+    activityIndicator.stopAnimating()
+}
 }
